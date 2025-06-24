e8b03860a90e8ac67b086c70ed8d76c31e2d92c4
Jayleen Chen <ly.chen.2023@mitb.smu.edu.sg>
2025-06-25 01:30:08 +0800

    update dag

--- MLEAssignment2/dags/dag.py
+++ MLEAssignment2/dags/dag.py
@@ -21,8 +21,8 @@ with DAG(
     default_args=default_args,
     description='data pipeline run once a month',
     schedule='0 0 1 * *',  # At 00:00 on day-of-month 1: when you want to run (translate to cron)
-    start_date=datetime(2023, 1, 1),
-    end_date=datetime(2024, 12, 1),
+    start_date=datetime(2022, 1, 1),
+    # end_date=datetime(2024, 12, 1),
     catchup=True,
 
 ) as dag:
@@ -46,7 +46,7 @@ with DAG(
             '--task bronze_label'
         ),
     )
-    # ds stands for date. this is where to input the date based on the schedule provided above. 
+
     # silver_label_store = DummyOperator(task_id="silver_label_store")
     silver_label_store = BashOperator(
         task_id='run_silver_label_store',
@@ -68,48 +68,79 @@ with DAG(
             '--task gold_label'
         ),
     )
-
     label_store_completed = DummyOperator(task_id="label_store_completed")
 
     # --- feature store --- chaining multiple bronze table to silver table
-    dep_check_source_data_bronze_1 = DummyOperator(task_id="dep_check_source_data_bronze_1")
-    dep_check_source_data_bronze_2 = DummyOperator(task_id="dep_check_source_data_bronze_2")
-    dep_check_source_data_bronze_3 = DummyOperator(task_id="dep_check_source_data_bronze_3")
-
     bronze_clickstream_task = BashOperator(
     task_id='bronze_clickstream',
-    bash_command='cd /opt/airflow/scripts && python3 feature_processing.py --snapshotdate "{{ ds }}" --task bronze_clickstream'
+    bash_command=(
+        'cd /opt/airflow/scripts && '
+        'python3 feature_processing.py '
+        '--snapshotdate "{{ ds }}" '
+        '--task bronze_clickstream'
+        ),
     )
 
     bronze_attributes_task = BashOperator(
         task_id='bronze_attributes', 
-        bash_command='cd /opt/airflow/scripts && python3 feature_processing.py --snapshotdate "{{ ds }}" --task bronze_attributes'
+        bash_command=(
+            'cd /opt/airflow/scripts && '
+            'python3 feature_processing.py'
+            ' --snapshotdate "{{ ds }}" '
+            '--task bronze_attributes'
+        )
     )
 
     bronze_financials_task = BashOperator(
         task_id='bronze_financials',
-        bash_command='cd /opt/airflow/scripts && python3 feature_processing.py --snapshotdate "{{ ds }}" --task bronze_financials'
+        bash_command=(
+            'cd /opt/airflow/scripts && '
+            'python3 feature_processing.py '
+            '--snapshotdate "{{ ds }}" '
+            '--task bronze_financials'
+        )
     )
 
     silver_clickstream_task = BashOperator(
     task_id='silver_clickstream',
-    bash_command='cd /opt/airflow/scripts && python3 feature_processing.py --snapshotdate "{{ ds }}" --task silver_clickstream'
+    bash_command=(
+        'cd /opt/airflow/scripts && '
+        'python3 feature_processing.py '
+        '--snapshotdate "{{ ds }}" '
+        '--task silver_clickstream'
+        )
     )
 
     silver_attributes_task = BashOperator(
         task_id='silver_attributes', 
-        bash_command='cd /opt/airflow/scripts && python3 feature_processing.py --snapshotdate "{{ ds }}" --task silver_attributes'
+        bash_command=(
+            'cd /opt/airflow/scripts && '
+            'python3 feature_processing.py '
+            '--snapshotdate "{{ ds }}" '
+            '--task silver_attributes'
+        )
     )
 
     silver_financials_task = BashOperator(
         task_id='silver_financials',
-        bash_command='cd /opt/airflow/scripts && python3 feature_processing.py --snapshotdate "{{ ds }}" --task silver_financials'
+        bash_command=(
+            'cd /opt/airflow/scripts && '
+            'python3 feature_processing.py '
+            '--snapshotdate "{{ ds }}" '
+            '--task silver_financials'
+        )
     )
 
     # gold_feature_store = DummyOperator(task_id="gold_feature_store")
     gold_feature_store = BashOperator(
         task_id='gold_features',
-        bash_command='cd /opt/airflow/scripts && python3 feature_processing.py --snapshotdate "{{ ds }}" --task gold_features --type training'
+        bash_command=(
+            'cd /opt/airflow/scripts && '
+            'python3 feature_processing.py '
+            '--snapshotdate "{{ ds }}" '
+            '--task gold_features '
+            '--type inference'
+        )
     )
     bronze_processing_completed = DummyOperator(task_id="bronze_processing_completed")
     silver_processing_completed = DummyOperator(task_id="silver_processing_completed")
@@ -125,31 +156,49 @@ with DAG(
     dep_check_source_label_data >> bronze_label_store >> silver_label_store
 
     # Gold layer processing
-    dep_check_source_data_bronze_3 >> gold_feature_store
     gold_feature_store >> feature_store_completed
     gold_label_store >> label_store_completed
 
     model_train = BashOperator(
         task_id='training_model',
-        bash_command = 'cd /opt/airflow/scripts && python3 model_training.py --job train --snapshotdate "{{ ds }}"'
+        bash_command =(
+            'cd /opt/airflow/scripts && '
+            'python3 model_training.py '
+            '--job train '
+            '--snapshotdate "{{ ds }}"'
+        ) 
     )
 
     deploy_model = BashOperator(
         task_id='deploy_best',
-        bash_command = 'cd /opt/airflow/scripts && python3 model_training.py --job deploy --snapshotdate "{{ ds }}"'
+        bash_command = (
+            'cd /opt/airflow/scripts && '
+            'python3 model_training.py '
+            '--job deploy '
+            '--snapshotdate "{{ ds }}"'
+        )
     )
 
      # --- model inference ---
     model_inference_start = DummyOperator(task_id="batch_inference_start")
     model_inference = BashOperator(
         task_id="model_inference",
-        bash_command = 'cd /opt/airflow/scripts && python3 model_inference.py --snapshotdate "{{ ds }}"'
+        bash_command = (
+            'cd /opt/airflow/scripts && '
+            'python3 model_inference.py '
+            '--snapshotdate "{{ ds }}"'
+        )
     )
 
     online_features = BashOperator(
         task_id='gold_features',
-        bash_command='cd /opt/airflow/scripts && python3 feature_processing.py --snapshotdate "{{ ds }}" --task gold_features --type inference', 
-
+        bash_command=(
+            'cd /opt/airflow/scripts && '
+            'python3 feature_processing.py '
+            '--snapshotdate "{{ ds }}" '
+            '--task gold_features '
+            '--type inference'
+        )
     )
 
     model_inference_completed = DummyOperator(task_id="model_inference_completed")
